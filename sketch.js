993783c39ee57500debb8fa756f16dd7257bb0c9
Matthew Dulworth <mhdulworth@gmail.com>
2020-01-17 21:31:54 -0600

    added terrain variation

--- sketch.js
+++ sketch.js
@@ -2,8 +2,9 @@
 
 // ----- vars ----- //
 let canvas;
-let rows, columns;
+let rows, columns, terrain;
 let scale = 20;
+
 
 // ----- setup ----- //
 function setup() {
@@ -13,8 +14,10 @@ function setup() {
    canvas.style('display: block;');
 
    // get number of rows and columns 
-   columns = width / scale;
-   rows = height / scale;
+   columns = Math.floor(width / scale);
+   rows = Math.floor(height * 1.5 / scale);
+
+   terrain = generateTerrain(rows, columns, 10);
 }
 
 // ----- resize ----- // 
@@ -25,24 +28,43 @@ function windowResized() {
 // ----- draw ----- // 
 function draw() {
    background(0);
-   translate(-width/2, -height/2, 0);
-   rotateX(PI/3);
-   drawMesh();
+   drawMesh(terrain);
 }
 
+// ----- init terrain ----- // 
+function generateTerrain(rows, columns, offset) {
 
-// ----- create grid ----- // 
-function drawMesh() {
-   push();
-   stroke(255);
-   noFill();
+   let terrain = new Array(rows);
+   for (let i = 0; i < terrain.length; i++) {
+      terrain[i] = new Array(columns);
+   }
 
    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < columns; x++) {
+         terrain[y][x] = random(-offset, offset);
+      }
+   }
+
+   return terrain;
+}
+
+// ----- create grid ----- // 
+function drawMesh(terrain) {
+   push();
+
+   // setup
+   stroke(255);
+   noFill();
+   rotateX(PI / 3);
+   translate(-width / 2, -height / 2, 0);
+
+   // draw triangle strips
+   for (let y = 0; y < rows - 1; y++) {
 
       beginShape(TRIANGLE_STRIP);
       for (let x = 0; x < columns; x++) {
-         vertex(x*scale, y*scale);
-         vertex(x*scale, (y + 1)*scale);
+         vertex(x * scale, y * scale, terrain[y][x]);
+         vertex(x * scale, (y + 1) * scale, terrain[y + 1][x]);
       }
       endShape();
    }
